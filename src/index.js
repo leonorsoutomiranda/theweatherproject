d15f7c5e67076a9da964433bae7b093227b1ccc7
leonorsoutomiranda <59849490+leonorsoutomiranda@users.noreply.github.com>
2020-03-22 19:52:39 +0000

    Including forecast in js

--- src/index.js
+++ src/index.js
@@ -90,6 +90,20 @@ function displaycurrentday() {
 }
 displaycurrentday();
 
+//Format hours
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}: ${minutes}`;
+}
+
 //Search Location
 function search(event) {
   event.preventDefault();
@@ -100,13 +114,11 @@ function search(event) {
 
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityinput}&appid=5d25e265a18f244a8bfebb37b4d7164b&&units=metric`;
   axios.get(url).then(displaytemperature);
+
+  url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityinput}&appid=5d25e265a18f244a8bfebb37b4d7164b&&units=metric`;
+  axios.get(url).then(displayforecast);
 }
 //Display temperature
-//icon: "fas fa-cloud-sun" = 02d.png , 02n.png, 04d.png, 04n.png , 03d.png, 03n.png ;
-//icon: "fas fa-cloud-showers-heavy" = 09d.png , 09n.png, 10d.png, 10n.png;
-//icon: "fas fa-sun" = 01d.png , 01n.png;
-//icon: "far fa-snowflake" = 13d.png , 13n.png;
-//icon: "fas fa-bolt"=11d.png , 11n.png
 function displaytemperature(response) {
   celsiusT = response.data.main.temp;
   let wind = response.data.wind.speed;
@@ -147,17 +159,71 @@ function displaytemperature(response) {
   document.querySelector("#wind").innerHTML = wind;
   document.querySelector("#humidity").innerHTML = humidity;
 }
+
+//Display forecast
+function displayforecast(response) {
+  document.querySelector("#next-hours").innerHTML = "Next hours forecast:";
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  let forecastIcon = null;
+  let forecastIconRef = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+    forecastIconRef = response.data.list[index].weather[0].icon;
+    if (
+      forecastIconRef == "02d" ||
+      forecastIconRef == "02n" ||
+      forecastIconRef == "04d" ||
+      forecastIconRef == "04n" ||
+      forecastIconRef == "03d" ||
+      forecastIconRef == "03n"
+    ) {
+      forecastIcon = "fas fa-cloud-sun";
+    } else if (
+      forecastIconRef == "09d" ||
+      forecastIconRef == "09n" ||
+      forecastIconRef == "10d" ||
+      forecastIconRef == "10n"
+    ) {
+      forecastIcon = "fas fa-cloud-showers-heavy";
+    } else if (forecastIconRef == "13d" || forecastIconRef == "13n") {
+      forecastIcon = "fas fa-snowflake";
+    } else if (forecastIconRef == "11d" || forecastIconRef == "11n") {
+      forecastIcon = "fas fa-bolt";
+    } else {
+      forecastIcon = "fas fa-sun";
+    }
+    celsiusMax = Math.round(forecast.main.temp_max);
+    celsiusMin = Math.round(forecast.main.temp_min);
+    forecastElement.innerHTML += `<div class="col-sm"
+                  >
+                </div>
+                <div class="col-sm"
+                  ><div class="nextdays">
+                      ${formatHours(forecast.dt * 1000)}
+                  </div>
+                  <i class="${forecastIcon}"></i>
+                  <div class="maxmin">Max: ${celsiusMax} º<br /> Min: ${celsiusMin} º</div>
+                </div>`;
+  }
+}
+
 //Search box
 let searchform = document.querySelector("#location-form");
 searchform.addEventListener("submit", search);
 searchform.addEventListener("click", search);
 
-//Display current temperature
+//Display current temperature & current loc forecast
 function showcurrentweather(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5d25e265a18f244a8bfebb37b4d7164b&&units=metric`;
   axios.get(url).then(displaycurrentloctemperature);
+
+  url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5d25e265a18f244a8bfebb37b4d7164b&&units=metric`;
+  axios.get(url).then(displaycurrentlocforecast);
 }
 
 function getCurrentPosition() {
@@ -207,25 +273,80 @@ function displaycurrentloctemperature(response) {
   }
 }
 
+function displaycurrentlocforecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  let forecastIcon = null;
+  let forecastIconRef = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+    forecastIconRef = response.data.list[index].weather[0].icon;
+    if (
+      forecastIconRef == "02d" ||
+      forecastIconRef == "02n" ||
+      forecastIconRef == "04d" ||
+      forecastIconRef == "04n" ||
+      forecastIconRef == "03d" ||
+      forecastIconRef == "03n"
+    ) {
+      forecastIcon = "fas fa-cloud-sun";
+    } else if (
+      forecastIconRef == "09d" ||
+      forecastIconRef == "09n" ||
+      forecastIconRef == "10d" ||
+      forecastIconRef == "10n"
+    ) {
+      forecastIcon = "fas fa-cloud-showers-heavy";
+    } else if (forecastIconRef == "13d" || forecastIconRef == "13n") {
+      forecastIcon = "fas fa-snowflake";
+    } else if (forecastIconRef == "11d" || forecastIconRef == "11n") {
+      forecastIcon = "fas fa-bolt";
+    } else {
+      forecastIcon = "fas fa-sun";
+    }
+    celsiusMax = Math.round(forecast.main.temp_max);
+    celsiusMin = Math.round(forecast.main.temp_min);
+    forecastElement.innerHTML += `<div class="col-sm"
+                  >
+                </div>
+                <div class="col-sm"
+                  ><div class="nextdays">
+                      ${formatHours(forecast.dt * 1000)}
+                  </div>
+                  <i class="${forecastIcon}"></i>
+                  <div class="maxmin">Max: ${celsiusMax} º<br /> Min: ${celsiusMin} º</div>
+                </div>`;
+  }
+}
+
+function changeText() {
+  document.querySelector("#next-hours").innerHTML = "Next hours forecast:";
+}
+
 let currentlocbutton = document.querySelector("#go-home");
 currentlocbutton.addEventListener("click", getCurrentPosition);
+currentlocbutton.addEventListener("click", changeText);
 
 //Fahrenheit
 function displayFahr(event) {
   event.preventDefault();
   let fahr = (celsiusT * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahr) + "º F";
+  temperatureElement.innerHTML = Math.round(fahr) + "ºF";
 }
 
 //Celsius
 function displayCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusT) + "º C";
+  temperatureElement.innerHTML = Math.round(celsiusT) + "ºC";
 }
 
 let celsiusT = null;
+let celsiusMax = null;
+let celsiusMin = null;
 
 let fahrLink = document.querySelector("#f-link");
 fahrLink.addEventListener("click", displayFahr);
